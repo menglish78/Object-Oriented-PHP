e318a98b5e7d7835c97ef9c8e765b7d2b691c81d
Matt English <menglish78@bellsouth.net>
2016-06-14 07:16:18 -0400

    Added classes folder and recipes file.  Added the initial class and some functions.

---
+++ classes/recipes.php
@@ -0,0 +1,27 @@
+<?php
+
+class Recipe
+{
+  public $title;
+  public $ingredients = array();
+  public $instructions = array();
+  public $yield;
+  public $tag = array();
+  public $source = "Alena Holligan";
+
+  public function displayRecipe()
+  {
+    return $this->title . "by" . $this->source;
+  }
+}
+
+$recipe1 = new Recipe();
+$recipe1->source = "Grandma Holligan";
+$recipe1->title = "My First Recipe";
+
+$recipe2 = new Recipe();
+$recipe2->source = "Betty Crocker";
+$recipe2->title = "My Second Recipe";
+
+echo $recipe1->displayRecipe();
+echo $recipe2->displayRecipe();
